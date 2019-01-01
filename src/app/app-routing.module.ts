fef5e727b8dc1ea24fb039ad2cee156ea7305554
hatemBouhajja <hatembouhajja@gmail.com>
2019-01-01 19:50:19 +0100

    Update view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { ProfileSetupComponent } from './profile-setup/profile-setup.component';
 const routes: Routes = [
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'myprofile', component: ProfileSetupComponent }
 ];
 
 @NgModule({
